4f38432d1e451683dce4737eccb2a37631c3866d
israelBT232 <166003735+israelBT232@users.noreply.github.com>
2024-05-29 11:24:02 +0300

    Create Func.H

---
+++ Libs/Data_structures/Func/Func.H
@@ -0,0 +1,17 @@
+
+
+#ifndef UNTITLED3_FUNC_H
+#define UNTITLED3_FUNC_H
+
+int Task1();
+int Task2();
+int Task3();
+int Task4();
+void Task5(const char* filename);
+void Task6(float x, const char *filename);
+void Task7(const char *filename, int i);
+void Task8(const char *filename, int n);
+void Task9(char* filename, int n);
+void Task10(char* fileF, char* fileG);
+
+#endif //UNTITLED3_FUNC_H
